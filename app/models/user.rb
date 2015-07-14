a849b9d344fc9fa8c489d3e5937c5636681c5ad9
zhangmyihua <michaelzhang2@msn.com>
2015-07-15 01:44:19 +0800

    handing booking over to chris

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
 	validates :username, presence: true
 	validates :time_zone, presence: true
 	has_one :teacher, dependent: :destroy
+  has_many :bookings, foreign_key: :student_id
+  
 end
