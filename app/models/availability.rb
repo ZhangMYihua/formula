4689d75c149f23f3ba03e872737c342e4bc3f4fa
zhangmyihua <michaelzhang2@msn.com>
2015-07-15 01:23:35 +0800

    adding associations between models and booking

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,5 +1,6 @@
 class Availability < ActiveRecord::Base
 	belongs_to :teacher
-
+  has_many :bookings
+  
   
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,5 @@
 class Booking < ActiveRecord::Base
+  belongs_to :teacher
+  belongs_to :student, class_name: 'User'
+  belongs_to :availability
 end
